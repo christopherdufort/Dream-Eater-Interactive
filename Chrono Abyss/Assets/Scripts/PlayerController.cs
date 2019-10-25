a0dd75f1985ad37a73ebfc3834ab3a54432e20d1
OmarAlFarajat <40572852+OmarAlFarajat@users.noreply.github.com>
2019-10-25 12:35:15 -0400

    Running Animations, Acceleration, Code Clean Up
    \Scripts\PlayerController.cs
    -- Grouped variables by type under each heading.
    -- Renamed rb to rigidBody
    -- Added acceleration component to movement
    -- movementDirection and lookAtDirection were redundant
    -- Cleaned up code as per Gab's suggestions
    Unity stuff:
    -- Restructured folders for assets
    -- Added Player running sprites and animations, updated blend tree.
    -- Player is now a prefab, so the team can simply drag-and-drop Player into their scenes for testing.
    .gitignore:
    -- Excluding Build folder.

--- Chrono Abyss/Assets/Scripts/PlayerController.cs
+++ Chrono Abyss/Assets/Scripts/PlayerController.cs
@@ -8,21 +8,21 @@ public class PlayerController : MonoBehaviour
     [Space]
     [Header("Base attributes:")]
     public float PLAYER_BASE_SPEED = 2.5f;
+    public float PLAYER_BASE_ACCELERATION = 0.5f;
     public float BULLET_BASE_SPEED = 5.0f;
     public float BULLET_DURATION = 3.0f;
     public float SLASH_DURATION = 0.125f;
 
     [Space]
     [Header("Character Statistics:")]
-    public Vector2 movementDirection;
-    public float movementSpeed;
-    public Vector2 aimDirection;
     public bool isShooting;
-    Vector2 lookAtDirection;
+    public float movementSpeed;
+    public Vector2 movementDirection;
+    public Vector2 aimDirection;
 
     [Space]
     [Header("Component References:")]
-    public Rigidbody2D rb;
+    public Rigidbody2D rigidBody;
     public Animator animator;
     public GameObject crosshair;
 
@@ -31,8 +31,9 @@ public class PlayerController : MonoBehaviour
     public GameObject bulletPrefab;
     public GameObject slashPrefab;
 
-    // Slash reference
+    // Private attributes
     private GameObject slash;
+    private float acceleratedSpeed;
 
     // Update stack
     void Update()
@@ -47,19 +48,29 @@ public class PlayerController : MonoBehaviour
 
     void CheckInputs()
     {
-        // Get a movement direction vector from user input and normalize it. Get speed from this vector. 
-        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        movementDirection.Normalize();
-        movementSpeed = Mathf.Clamp(movementDirection.magnitude, 0.0f, 1.0f);
-        
+        // How long the player has been accelerating
+        acceleratedSpeed += Mathf.Clamp(Time.deltaTime * PLAYER_BASE_ACCELERATION, 0.0f, 1.0f);
+            
         // If player is moving
-        if (movementDirection != Vector2.zero)
-            // Get a look-at vector in the direction of where the player was last moving (already normalized). 
-            lookAtDirection = movementDirection;
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            // Get a movement direction vector from user input and normalize it. Get speed from this vector. 
+            movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            movementDirection.Normalize();
+            movementSpeed = Mathf.Clamp(acceleratedSpeed, 0.0f, 1.0f);
+        }
+        // If player is standing still
+        else
+        {
+            // Reset accleration timer and set movement speed to zero
+            acceleratedSpeed = 0f;
+            movementSpeed = 0;
+        }          
 
         // Get the aim direction vector from target position to player position
         aimDirection = Camera.main.ScreenToWorldPoint((Vector2)Input.mousePosition);
         aimDirection = aimDirection - (Vector2)transform.position; 
+
         // isShooting is true when Fire1 button is pressed
         isShooting = Input.GetButtonUp("Fire1");
     }
@@ -67,10 +78,10 @@ public class PlayerController : MonoBehaviour
     void Move()
     {
         // Move the player's rigid body in the movement direction (based on user input)
-        rb.velocity = movementDirection * movementSpeed * PLAYER_BASE_SPEED;
+        rigidBody.velocity = movementDirection * movementSpeed * PLAYER_BASE_SPEED;
         // If Player is slashing (a slash prefab exists), then keep the slash in front of Player at all times
         if (slash != null)
-            slash.GetComponent<Rigidbody2D>().position = lookAtDirection*0.5f + (Vector2)transform.position;
+            slash.GetComponent<Rigidbody2D>().position = movementDirection*0.5f + (Vector2)transform.position;
     }
 
     void Animate()
@@ -110,25 +121,26 @@ public class PlayerController : MonoBehaviour
 
     void Slash()
     {
-        if (Input.GetButton("Jump") == true) 
+        if (Input.GetButton("Jump"))
             // Prevents user from attacking again until the slash is complete
             if (slash == null)
+            {
                 // If the player is moving
                 if (movementDirection != Vector2.zero)
                 {
                     // Instantiate slash game object
                     slash = Instantiate(slashPrefab, movementDirection * 0.5f + (Vector2)transform.position, Quaternion.identity);
-                    // Destroy slash object after some duration
-                    Destroy(slash, SLASH_DURATION);
                 }
                 // If the player is standing still
                 else
                 {
-                    // Unlike above, this instantiation uses different vectors to determine where the prefab will spawn to account for the player standing still
-                    slash = Instantiate(slashPrefab, lookAtDirection * 0.5f + (Vector2)transform.position, Quaternion.identity);
-                    // In this case, slash prefab does not get a velocity from the player, since they are standing still.
-                    Destroy(slash, SLASH_DURATION);
+                    //Unlike above, this instantiation uses different vectors to determine where the prefab will spawn to account for the player standing still.
+                    //In this case, slash prefab does not get a velocity from the player, since they are standing still.
+                    slash = Instantiate(slashPrefab, movementDirection * 0.5f + (Vector2)transform.position, Quaternion.identity);
                 }
+                // Destroy slash object after some duration
+                Destroy(slash, SLASH_DURATION);
+            }
     }
 
 }
