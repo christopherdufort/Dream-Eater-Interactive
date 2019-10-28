3be04e6409d2e3a8c2dd025bd3558af7178af9ae
Christopher Dufort <christopher.dufort@outlook.com>
2019-10-27 21:35:20 -0400

    added precural level builder

---
+++ Chrono Abyss/Assets/Scripts/Destroyer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Purpose for this class is to prevent anything from spawning ontop of the player in the starting room.
+ * Also served the purpose of preventing the starting room from being closed off.
+ * 
+ */
+public class Destroyer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("RoomSpawnPoint"))
+        {
+            other.GetComponent<RoomSpawner>().preventSpawn();
+            Destroy(other.gameObject);
+        }
+    }
+}
