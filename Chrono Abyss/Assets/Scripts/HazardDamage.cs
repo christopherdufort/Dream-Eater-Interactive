29c92b908aa05074115522fc26f15132d384fcd3
Natalia Whiteley <nataliawhiteley11@gmail.com>
2019-11-14 18:08:56 -0500

    Added chest and hazard prefabs.

---
+++ Chrono Abyss/Assets/Scripts/HazardDamage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardDamage : MonoBehaviour
+{
+    public int damage = 1;
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // do damage
+        }
+    }
+}
