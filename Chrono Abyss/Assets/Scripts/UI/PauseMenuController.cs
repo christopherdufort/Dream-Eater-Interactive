3bf8a009cff8cf4633e30be1c77fabd71fff00ec
Natalia Whiteley <nataliawhiteley11@gmail.com>
2019-11-16 14:41:43 -0500

    Ui (#51)
    * Added sprites for game UI.
    
    Added UI sprites.
    Created UI test scene.
    Started creating in game UI.
    
    * Added pause screen and health bar.
    
    * Added start menu and pause screen.

---
+++ Chrono Abyss/Assets/Scripts/UI/PauseMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public void resumeGame()
+    {
+        // turn off canvas and set time scale to 1.0
+    }
+
+    public void quitToMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+}
