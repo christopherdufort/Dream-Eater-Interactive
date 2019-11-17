94513d7f9f2f977680b78edb659e591d94995b45
Christopher Dufort <christopher.dufort@outlook.com>
2019-11-17 16:47:59 -0500

    starting changes

---
+++ Chrono Abyss/Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerScript : MonoBehaviour
+{
+    bool gamePaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gamePaused = false;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape))
+        {
+            // Pause Game.
+            gamePaused = !gamePaused;
+
+        }
+    }
+}
+
+enum FloorTheme
+{
+    Lava,
+    Ice,
+    Dungeon,
+    Desert
+}
