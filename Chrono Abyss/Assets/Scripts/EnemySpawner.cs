1d39a05b952bf00dc286827cc10db461cfcd24e8
rng350 <rnguyen350@gmail.com>
2019-11-18 14:47:07 -0500

    completed func for checking overlapping colliders

--- Chrono Abyss/Assets/Scripts/EnemySpawner.cs
+++ Chrono Abyss/Assets/Scripts/EnemySpawner.cs
@@ -41,18 +41,24 @@ public class EnemySpawner : MonoBehaviour
 	bool OverlapsACollider(Vector3 spawnPosition, GameObject enemyToSpawn)
 	{
 		BoxCollider2D enemyToSpawnCollider = enemyToSpawn.GetComponent<BoxCollider2D>();
+		Vector2 enemyToSpawnExtents = enemyToSpawnCollider.bounds.extents;
+		Vector2 enemyToSpawnUpper = new Vector2(spawnPosition.x + enemyToSpawnExtents.x, spawnPosition.y + enemyToSpawnExtents.y);
+		Vector2 enemyToSpawnLower = new Vector2(spawnPosition.x - enemyToSpawnExtents.x, spawnPosition.y - enemyToSpawnExtents.y);
 
 		Collider2D[] curColliders = Physics2D.OverlapBoxAll(transform.position, new Vector2(2*spawnValues.x, 2*spawnValues.y), 0f);
 		foreach (Collider2D collider in curColliders)
 		{
-			Vector3 colliderCenter = collider.bounds.center;
-			float horizontalExtent = collider.bounds.extents.x;
-			float verticalExtent = collider.bounds.extents.y;
+			Vector2 colliderCenter = collider.bounds.center;
+			Vector2 colliderExtents = collider.bounds.extents;
 
-			Vector2 upper = new Vector2(colliderCenter.x + horizontalExtent, colliderCenter.y + verticalExtent);
-			Vector2 lower = new Vector2(colliderCenter.x - horizontalExtent, colliderCenter.y - verticalExtent);
+			Vector2 colUpper = new Vector2(colliderCenter.x + colliderExtents.x, colliderCenter.y + colliderExtents.y);
+			Vector2 colLower = new Vector2(colliderCenter.x - colliderExtents.x, colliderCenter.y - colliderExtents.y);
 
-			if ((spawnPosition.x <= upper.x) && (spawnPosition.x >= lower.x) && (spawnPosition.y <= upper.y) && (spawnPosition.y >= lower.y))
+			if ((spawnPosition.x <= colUpper.x) && (spawnPosition.x >= colLower.x) && (spawnPosition.y <= colUpper.y) && (spawnPosition.y >= colLower.y))
+			{
+				return true;
+			}
+			if ((colliderCenter.x <= enemyToSpawnUpper.x) && (colliderCenter.x >= enemyToSpawnLower.x) && (colliderCenter.y <= enemyToSpawnUpper.y) && (colliderCenter.y >= enemyToSpawnLower.y))
 			{
 				return true;
 			}
