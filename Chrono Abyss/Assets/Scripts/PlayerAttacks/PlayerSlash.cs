48fff467aa50c5bfad2bf1a4e4c19227794a4261
Natalia Whiteley <nataliawhiteley11@gmail.com>
2019-11-24 17:41:14 -0500

    Fixed sword animations and position.

--- Chrono Abyss/Assets/Scripts/PlayerAttacks/PlayerSlash.cs
+++ Chrono Abyss/Assets/Scripts/PlayerAttacks/PlayerSlash.cs
@@ -1,8 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerSlash : MonoBehaviour
 {
 	public float attackValue;
+	public Animator anim;
+
+	private Vector2 dir;
+	
+	private void Start()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	private void Update()
+	{
+		// change pos to dir of player
+		transform.localPosition = new Vector3(dir.x * 0.8f, dir.y * 0.8f, 1);
+
+		// flip sword if left or down
+		float scaleX = dir.x > 0.5f ? -1.0f : 1.0f;
+		transform.localScale = new Vector3(scaleX * 0.7f, 0.7f, 1);
+	}
+
+	public void setDir(Vector2 newDir)
+	{
+		dir = newDir;
+	}
 }
