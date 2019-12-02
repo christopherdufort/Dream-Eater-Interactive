4f4a4d735f7258fb665654ee944e53cbb54c88e7
Christopher Dufort <christopher.dufort@outlook.com>
2019-12-01 21:53:33 -0500

    updated ui added buttons

---
+++ Chrono Abyss/Assets/LevelUpScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelUpScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //On button click check if you have enough money
+    //Detract money
+    //Increment Total level + individual stat
+    //Upgrade button and level text
+}
