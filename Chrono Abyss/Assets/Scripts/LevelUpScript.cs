772ec654da354e6cdb0f65755085a373715099c1
Christopher Dufort <christopher.dufort@outlook.com>
2019-12-02 23:22:42 -0500

    fixes level up

--- Chrono Abyss/Assets/Scripts/LevelUpScript.cs
+++ Chrono Abyss/Assets/Scripts/LevelUpScript.cs
@@ -101,10 +101,13 @@ public class LevelUpScript : MonoBehaviour
         enduranceLevel = GameObject.Find("EnduranceLevelText").GetComponent<Text>();
         nextEnduranceLevel = GameObject.Find("NextEnduranceLevelCostText").GetComponent<Text>();
 
+        gameController = GameObject.Find("GameController");
         LoadPlayerData();
 
         totalLevelCount.text = totalLevel.ToString();
+        Debug.Log("totalCoinCount.text " + totalCoinCount.text);
         totalCoinCount.text = totalGold.ToString();
+        Debug.Log("totalCoinCount.text after " + totalCoinCount.text);
 
 
     }
@@ -112,7 +115,7 @@ public class LevelUpScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     IEnumerator CollectPlayerForLevelUp()
@@ -128,8 +131,8 @@ public class LevelUpScript : MonoBehaviour
 
     public void LoadPlayerData()
     {
-        totalGold = player.GetComponent<PlayerController>().goldCollected;
 
+        totalGold = player.GetComponent<PlayerController>().goldCollected;
         totalLevel = gameController.GetComponent<GameController>().playerData.PlayerLevel;
         vitality = gameController.GetComponent<GameController>().playerData.Vitality;
         attunement = gameController.GetComponent<GameController>().playerData.Attunement;
