d217b0e10c9e297adb1d960fe82b3de37a5c18f6
Christopher Dufort <christopher.dufort@outlook.com>
2019-12-02 21:20:55 -0500

    added final boss portal and final boss room, with unique final boss controller

---
+++ Chrono Abyss/Assets/FinalBossController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalBossController : MonoBehaviour
+{
+    [SerializeField] Vector3 playerSpawnPosition = new Vector3(0f, -5f, 0f);
+    public GameObject[] bossPrefabs; // Only add Borgrimon here
+    private int randomBossPosition;
+    private GameObject bossObj;
+
+    private void Start()
+    {
+        StartCoroutine("SetupBossFight");
+    }
+
+    IEnumerator SetupBossFight()
+    {
+        // bring over player
+        while (GameObject.FindGameObjectWithTag("Player") == null)
+        {
+            yield return null;
+        }
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        player.transform.position = playerSpawnPosition;
+
+        // bring over boss
+        randomBossPosition = Random.Range(0, bossPrefabs.Length);
+        bossObj = Instantiate(bossPrefabs[randomBossPosition], transform.position, Quaternion.identity);
+    }
+
+    public void BossDied()
+    {
+        // You win go outside...
+    }
+}
